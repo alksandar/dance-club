a2cc38358d6d0928ce604271f8cc384480cb5240
Aleksandar-Sasa Calic <sc@payuca.com>
2019-05-15 20:28:19 +0200

    - Implement config.

---
+++ src/utils/config.js
@@ -0,0 +1,31 @@
+const convict = require('convict');
+
+const config = convict({
+    env: {
+        doc: 'The server environment.',
+        format: ['production', 'development', 'local'],
+        default: 'local',
+        env: 'ENV'
+    },
+    port: {
+        doc: 'The port to bind.',
+        format: 'port',
+        default: 4000,
+        env: 'APP_PORT',
+    },
+
+    db: {
+        ssl: {
+            doc: 'Support for ssl',
+            format: Boolean,
+            default: false,
+            env: 'DB_SSL'
+        },
+        url: {
+            doc: 'Database uri',
+            format: String,
+            default: null,
+            env: 'DB_URL'
+        }
+    },
+});
