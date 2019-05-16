dbd8c5723dd5f10d95cf0ae25c36d4bf3178a3bc
alksandar <sc@payuca.com>
2019-05-16 16:42:42 +0200

    - Add sync statements for tables.

--- src/models/tables.js
+++ src/models/tables.js
@@ -12,6 +12,12 @@ const Payment = require('./Payment')(DatabaseConnection.db, DataTypes);
 Group.hasMany(Dancer, { onDelete: 'set null', as: 'dancers' });
 Dancer.hasMany(Payment, { onDelete: 'set null', as: 'dancerPayments' });
 
+// if not exist, create table
+// force will drop the table and recreate it if any table is available
+Dancer.sync();
+Group.sync();
+Payment.sync();
+
 module.exports.Dancer = Dancer;
 module.exports.Group = Group;
 module.exports.Payment = Payment;
